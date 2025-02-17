9933c04a8b65a2c64828c11616ec7daa7501e195
Eduardo Santana <ezambomsantana@gmail.com>
2025-02-17 18:53:32 -0300

    add methods

--- aula-02/projeto/src/main/java/org/example/Main.java
+++ aula-02/projeto/src/main/java/org/example/Main.java
@@ -1,20 +1,27 @@
 package org.example;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
+
         Pessoa p = new Pessoa();
-        p.nome = "Eduardo";
-        p.dataNascimento = LocalDate.now();
-        p.endereco = "Rua xyz";
+        p.setNome("Eduardo");
+        p.setDataNascimento(LocalDate.of(1986, 3,7));
+        p.setEndereco("Rua xyz");
+        int idade = p.calculaIdade();
+
+        System.out.println(idade);
         System.out.println(p);
 
         Pessoa p2 = new Pessoa();
-        p2.nome = "Luiz";
-        System.out.println(p.nome);
-        System.out.println(p2.nome);
+        p2.setNome("Luiz");
+        idade = p2.calculaIdade();
+        System.out.println(p.getNome());
+        System.out.println(p2.getNome());
 
         System.out.println(p2.equals(p));
 
@@ -23,13 +30,10 @@ public class Main {
         carro.ano = 2020;
         carro.modelo = "Onix";
 
-        p.carros.add(carro);
+        p.addCarro(carro);
+        p.addCarro(null);
 
-        System.out.println(p.carros.get(0).modelo);
 
-        for (Carro c : p.carros) {
-            System.out.println(c.modelo);
-        }
 
     }
 }
--- aula-02/projeto/src/main/java/org/example/Pessoa.java
+++ aula-02/projeto/src/main/java/org/example/Pessoa.java
@@ -1,14 +1,59 @@
 package org.example;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 
 public class Pessoa {
 
-    String nome;
-    LocalDate dataNascimento;
-    String endereco;
-    ArrayList<Carro> carros = new ArrayList<>();
+    private String nome;
+    private LocalDate dataNascimento;
+    private String endereco;
+    private ArrayList<Carro> carros = new ArrayList<>();
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void addCarro(Carro carro) {
+        if (carro != null && carro.ano > 2022) {
+            carros.add(carro);
+        }
+        calculaIdade();
+    }
+
+    public int calculaIdade() {
+        if (dataNascimento != null) {
+            return Period.between(dataNascimento, LocalDate.now()).getYears();
+        }
+
+        Carro carro = new Carro();
+        carro.setModelo("X");
+
+        addCarro(carro);
+    }
+
+
 
     @Override
     public String toString() {
